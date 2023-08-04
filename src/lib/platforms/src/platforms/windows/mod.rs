941222dd29f79caa9d27e94b5542e1d81be7b38a
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-04 23:15:11 +0100

    feat(backends/reexports): Add module for reexporting deps

--- src/lib/platforms/src/platforms/windows/mod.rs
+++ src/lib/platforms/src/platforms/windows/mod.rs
@@ -3,3 +3,8 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 //! Stub module (TODO) for the Windows backend.
+
+pub mod reexports {
+    //! Reexports of dependencies used in this backend.
+    pub use windows;
+}
