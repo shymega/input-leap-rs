f860a70824b084547ef25bfcf48c8e02e9775d10
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-04 23:15:11 +0100

    feat(backends/reexports): Add module for reexporting deps

--- src/lib/platforms/src/platforms/macos/mod.rs
+++ src/lib/platforms/src/platforms/macos/mod.rs
@@ -3,3 +3,10 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 //! Stub module (TODO) for the macOS backend.
+
+pub mod reexports {
+    //! Reexports of dependencies used in this backend.
+    pub use cocoa_foundation;
+    pub use core_foundation;
+    pub use core_graphics;
+}
--- src/lib/platforms/src/platforms/unix/mod.rs
+++ src/lib/platforms/src/platforms/unix/mod.rs
@@ -3,3 +3,9 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 //! Stub module (TODO) for the Unix backend. Excluding macOS.
+
+pub mod reexports {
+    //! Reexports of dependencies used in this backend.
+    pub use breadx;
+    pub use smithay;
+}
